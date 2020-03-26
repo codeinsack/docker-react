f810f7dd4ca9cd47a16858baec2eb7025589b25c
codeinsack <codeinsack@gmail.com>
2020-03-26 21:40:17 +0300

    change app

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Bye!
+          Hello from React!
         </p>
         <a
           className="App-link"
